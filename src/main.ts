e1064fb5a21433a9597a19d465ae9f462ee35592
Ethan Tan <imotekhx@gmail.com>
2020-09-28 21:47:11 +0800

    Modified main.ts for testing branch

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ChessGame } from './chess_general/chessGame';
 
-const chess = new ChessGame();
-chess.run();
+// const chess = new ChessGame();
+// chess.run();
+console.log("Trying Git and GitHub for the first time...");
